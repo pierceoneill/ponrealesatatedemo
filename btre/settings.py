c1c954c9df7ed328fd399080bd3296003ef04af4
Pierce O'Neill <pierceoneillky@gmail.com>
2023-09-25 14:08:33 +0100

    updated settings

--- btre/settings.py
+++ btre/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -80,15 +81,7 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ponredb',
-        'USER': 'postgres',
-        'PASSWORD': 'Wms0Fri2024!',
-        'HOST': 'localhost'
-    }
-}
+DATABASES = {'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)}
 
 
 # Password validation
